c06fe050d8c834a909e018ae2ccd7c26231162e7
SamLinux <oxedom@gmail.com>
2022-08-29 13:51:41 +0300

    COMPUTER CAN NOW ATTACK

--- src/js/player.js
+++ src/js/player.js
@@ -1,9 +1,24 @@
+const attackEnemy = (enemyGameBoard, cord) => { enemyGameBoard.receiveAttack(cord) }
+
 const PlayerFactory = () => {
 
-    const attackEnemy = (enemyGameBoard, cord) => { enemyGameBoard.receiveAttack(cord) }
+    let turn = true
 
 
     return { attackEnemy }
 }
 
-module.exports = { PlayerFactory }
+const ComputerPlayerFactory = () => {
+
+    const computerAttack = (enemyGameBoard) => {
+        let randomCord = Math.floor(Math.random() * 100) + 1
+
+        if (enemyGameBoard.getMissed().includes(randomCord)) {
+            computerAttack(enemyGameBoard)
+        }
+
+    }
+    return { computerAttack }
+}
+
+module.exports = { PlayerFactory, ComputerPlayerFactory }
