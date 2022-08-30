c2d897a27fd12bb649cf1574e84bdc921ddd09ba
SamLinux <oxedom@gmail.com>
2022-08-30 14:52:25 +0300

    board comp

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,11 +1,33 @@
+import './styles.css'
+import { pubsub } from './pubsub';
 export default (function () {
     'use strict';
     const content = document.getElementById('content')
 
-    for (let index = 0; index < 100; index++) {
-        const div = document.createElement('div')
-        div.innerText = 'amit'
-        content.append(div)
+
+    const createGameboard = () => {
+        let board = document.createElement('div')
+        board.classList.add('gameboard')
+        for (let index = 0; index < 100; index++) {
+
+            let cell = document.createElement('div')
+            cell.setAttribute('id', index + 1)
+            cell.classList.add('cell')
+            cell.addEventListener('click', (e) => {
+                pubsub.publish("cellClicked")
+            })
+            board.append(cell)
+        }
+        return board
+
 
     }
+    content.append(createGameboard())
+
+
+
+
+
+
+
 })();
