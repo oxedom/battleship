dbfd49689adae00645d7508b41e996c05f7d97a4
SamLinux <oxedom@gmail.com>
2022-08-30 15:23:25 +0300

    board comp

--- src/js/dom.js
+++ src/js/dom.js
@@ -5,24 +5,53 @@ export default (function () {
     const content = document.getElementById('content')
 
 
+    const arena = () => {
+        const arena = document.createElement('div')
+        arena.classList.add('arena')
+
+
+        return arena
+    }
+
     const createGameboard = () => {
         let board = document.createElement('div')
         board.classList.add('gameboard')
+
         for (let index = 0; index < 100; index++) {
 
             let cell = document.createElement('div')
             cell.setAttribute('id', index + 1)
-            cell.classList.add('cell')
-            cell.addEventListener('click', (e) => {
-                pubsub.publish("cellClicked")
-            })
+            cell.classList.add('cell'),
+                cell.addEventListener('click', (e) => { pubsub.publish("cellClicked", e) })
             board.append(cell)
         }
         return board
 
 
     }
-    content.append(createGameboard())
+
+    function changeCell(cell, boolean) {
+        if (boolean) {
+            cell.classList.add('hit')
+        }
+        else {
+            cell.classList.add('miss')
+        }
+    }
+
+
+    function init() {
+        let arenaElement = arena()
+        let gameboardOne = createGameboard()
+        let gameboardTwo = createGameboard()
+        arenaElement.append(gameboardOne, gameboardTwo)
+        return arenaElement
+    }
+
+    pubsub.subscribe('cellClicked', changeCell)
+
+
+    content.append(init())
 
 
 
