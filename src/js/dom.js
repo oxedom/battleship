4c3e016a4a08a092a226a2e0a87be955fba9e360
SamLinux <oxedom@gmail.com>
2022-08-31 16:07:52 +0300

    paint cells

--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,7 @@
 import './styles.css'
 import { pubsub } from './pubsub';
 
+
 export const dom = (function () {
     'use strict';
     let state =
@@ -8,20 +9,20 @@ export const dom = (function () {
         direction: 'row',
         selectedShip: undefined,
         playerBoardBuilt: false,
-
+        gameStart: false
 
     }
 
     const content = document.getElementById('content')
 
-    const main = () => {
+    function main() {
         const main = document.createElement('main')
         main.classList.add('main')
 
         return main
     }
 
-    const arena = () => {
+    function arena() {
         const arena = document.createElement('div')
         arena.classList.add('arena')
         return arena
@@ -37,6 +38,7 @@ export const dom = (function () {
             cell.classList.add('cell')
             board.append(cell)
             if (!state.playerBoardBuilt) {
+                cell.addEventListener('click', (e) => { pubsub.publish('cellClicked', e) })
                 cell.setAttribute('id', index + 1)
             }
         }
@@ -45,34 +47,6 @@ export const dom = (function () {
 
 
     }
-
-
-    const arsenal = () => {
-        const container = document.createElement('div')
-        container.setAttribute('id', 'arsenal')
-        container.classList.add('arsenal')
-        const ships = [ship(1), ship(2), ship(3), ship(4), ship(5)]
-        ships.forEach(ship => {
-            let shipLength = ship.id
-            const p = document.createElement('p')
-            p.innerText = shipLength
-            p.classList.add('shipLength')
-            ship.appendChild(p)
-            ship.addEventListener('click', () => {
-                ships.forEach(ship => ship.classList.remove('selected'))
-                ship.classList.add('selected')
-                pubsub.publish('selectedShip', ship.id)
-            })
-        })
-        container.append(...ships)
-
-        return container
-    }
-
-    pubsub.subscribe('selectedShip', setShip)
-
-    function setShip(shipLength) { state.selectedShip = shipLength }
-
 
     function footer() {
         let footer = document.createElement('footer')
@@ -86,6 +60,55 @@ export const dom = (function () {
         header.innerText = 'BATTLESHIP'
 
         return header
+    }
+
+
+    const arsenal = () => {
+        const container = document.createElement('div')
+        container.setAttribute('id', 'arsenal')
+        container.classList.add('arsenal')
+        const ships = [ship(1), ship(2), ship(3), ship(4), ship(5)]
+        ships.forEach(ship => {
+            let shipLength = ship.attributes[1].value
+
+            const p = document.createElement('p')
+            p.innerText = shipLength
+            p.classList.add('shipLength')
+            ship.appendChild(p)
+            ship.addEventListener('click', () => {
+                ships.forEach(ship => ship.classList.remove('selected'))
+                ship.classList.add('selected')
+                pubsub.publish('selectedShip', ship.attributes[1].value)
+            })
+        })
+        container.append(...ships)
+
+        return container
+    }
+
+    pubsub.subscribe('selectedShip', setShip)
+    pubsub.subscribe('cellClicked', handleCell)
+
+    function setShip(shipLength) { state.selectedShip = shipLength }
+
+    function handleCell(e) {
+        //WILL ACTIVATE IF GAME STARTED IS FALSE AND THERE IS A SHIP SELECTED
+        if (!state.gameStart && state.selectedShip) {
+            let clickedCell = parseInt(e.target.id)
+
+            let cellCords = []
+            for (let index = 0; index < state.selectedShip; index++) {
+                cellCords.push(clickedCell)
+                clickedCell = clickedCell + 1
+            }
+            paintCells(cellCords, state.selectedShip)
+        }
+    }
+
+    function paintCells(cellCords, amount) {
+        const cells = document.querySelectorAll('.cell').forEach(cell => cell.classList.remove('cellected'))
+        cellCords.forEach(cell => document.getElementById(`${cell}`).classList.add('cellected'))
+
     }
 
     function ship(value) {
@@ -113,7 +136,6 @@ export const dom = (function () {
             }
 
             ship.innerText = shipLeter
-            ship.setAttribute('id', value)
             ship.setAttribute('value', value)
             ship.setAttribute('draggable', true)
             return ship
@@ -123,8 +145,6 @@ export const dom = (function () {
         }
 
     }
-
-
 
 
     function init() {
