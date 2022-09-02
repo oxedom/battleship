2175540e0b03c779a0552bebddf4d9619a2a8046
Sam <aaaa@gmail.com>
2022-09-02 17:53:15 +0300

    refactor

--- src/js/tests/ship.test.js
+++ src/js/tests/ship.test.js
@@ -1,39 +1,13 @@
 const { ShipFactory } = require("../ship");
 
-describe("SET CORDS + HIT SHIP, CLEARCOARDS", () => {
-  const testShip = ShipFactory();
-  testShip.setCords([4, 10, 40, 10]);
-  test("Set Cords", () => {
-    expect(testShip.getCords()).toEqual([4, 10, 40, 10]);
-  });
-  test("hitShip ", () => {
-    expect(testShip.hitShip(4)).toBe(true);
-  });
+test("test", () => {
+  const testShip = ShipFactory()
+  testShip.setLength(4)
+  let answer = true
 
-  test("Clear Cords", () => {
-    testShip.clearCords();
-    expect(testShip.getCords()).toEqual([]);
-  });
-});
-describe("SINK SHIP", () => {
-  test("sinkShip", () => {
-    const sinkShip = ShipFactory();
-    sinkShip.setCords([5, 6, 7, 8]);
-    sinkShip.hitShip(5);
-    sinkShip.hitShip(6);
-    sinkShip.hitShip(7);
-    sinkShip.hitShip(8);
-    expect(sinkShip.getCords()).toEqual([5, 6, 7, 8]);
-    expect(sinkShip.isSunk()).toBe(true);
-  });
 
-  test("sinkShip 2", () => {
-    const sinkShip = ShipFactory();
-    sinkShip.setCords([5, 6, 7, 8]);
-    sinkShip.hitShip(5);
-    sinkShip.hitShip(7);
-    sinkShip.hitShip(8);
 
-    expect(sinkShip.isSunk()).toBe(false);
-  });
-});
+
+
+  expect(testShip.checkValid(1,2)).toEqual(answer)
+})
